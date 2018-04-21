7f9e9207dbbc9c892e60c3642b3cb97b23f61c6c
PeachTreeOath <peachtreeoath@gmail.com>
2018-04-21 01:30:19 -0600

    Lane selection

---
+++ Assets/Resources/Scripts/ResourceLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoader : Singleton<ResourceLoader>
+{
+    [HideInInspector]
+    public GameObject laneHighlight;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        laneHighlight = Resources.Load<GameObject>("Prefabs/LaneHighlight");
+    }
+}
