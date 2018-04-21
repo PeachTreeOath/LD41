534d166c72a551368ae0c38c5ff6d6c3c78d8b7b
PeachTreeOath <peachtreeoath@gmail.com>
2018-04-21 00:16:10 -0600

    Game manager

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+	// Use this for initialization
+	void Start () {
+        GlobalAttackCooldownTimer.instance.SetTickTime(3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
