0618ead780efe3c956fd26a541c9268417d1089c
mjeffery <michael.jeffery@gmail.com>
2018-04-22 13:48:09 -0400

    fixes to the AI

---
+++ Assets/Resources/Scripts/AiSpawnController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class AiSpawnController : MonoBehaviour  {
+
+    public List<Spawn> spawns;
+    public List<string> spawningPool;
+
+    public void Spawn() {
+        foreach(var id in spawningPool) {
+            SpawnById(id);
+        }
+    }
+
+    public void SpawnById(string id) {
+        var spawn = GetSpawnById(id);
+        if(spawn != null) {
+            spawn.Execute();
+        } else {
+            Debug.LogErrorFormat("Skipping unknown spawn with id '{0}'", id);
+        }
+    }
+
+    //TODO does this make sense?
+    public bool InterpretDirective(string directive, string argument) {
+        if (directive.ToLower() == "spawn pool") {
+            var matches = Regex.Matches(argument, @"([-+=])(a-z0-9_)\b");
+            foreach(Match match in matches) {
+                var op = match.Groups[1].Value;
+                var spawn = match.Groups[2].Value;
+
+                if(op == "=") {
+                    spawningPool.Clear();
+                    if(spawn.Length > 0) {
+                        spawningPool.Add(spawn);
+                    }
+                } else if(op == "+") {
+                    spawningPool.Add(spawn);
+                } else if(op == "-") {
+                    spawningPool.Remove(spawn);
+                }
+            }
+
+            return true;
+        } else if(directive.ToLower() == "spawn") {
+            var spawns = Regex.Split(argument, @"\s+");
+            foreach(var spawn in spawns) {
+                SpawnById(spawn);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private Spawn GetSpawnById(string id) {
+        foreach(var spawn in spawns) {
+            if (spawn.id == id)
+                return spawn;
+        }
+
+        return null;
+    }
+}
--- Assets/Resources/Scripts/GoblinAI.cs
+++ Assets/Resources/Scripts/GoblinAI.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 
+[RequireComponent(typeof(AiSpawnController))]
 public class GoblinAI : MonoBehaviour, IGlobalTimedObject {
 
     //TODO do we need curves for more than one value?
     //TODO make this not a behavior?
     private AiAnimationController spawnRateController;
+    private AiSpawnController spawnController;
 
     public Timer spawnTimer;
     public Spawn minionSpawn;
 
     public void ManualUpdate(float deltaTime) {
-        if (spawnTimer.Update(deltaTime)) minionSpawn.Execute();
+        if (spawnTimer.Update(deltaTime)) spawnController.Spawn();
     }
 
     void Start() {
+        spawnController = GetComponent<AiSpawnController>();
+
         spawnRateController = GetComponent<AiAnimationController>();
         spawnRateController.animValueEvent.AddListener(UpdateSpawnRate);
         spawnRateController.PlayAnimation("WarmUp");
