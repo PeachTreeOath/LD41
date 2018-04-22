69161f8507fa6acd5df88be4fb58d5e09d51a59a
mjeffery <michael.jeffery@gmail.com>
2018-04-22 17:20:18 -0400

    animation keyframes sorted by timestamp before use

--- Assets/Resources/Scripts/AiAnimation.cs
+++ Assets/Resources/Scripts/AiAnimation.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [Serializable]
-public class KeyFrame {
+public class KeyFrame : IComparable<KeyFrame> {
     public enum Type { Lerp, Step, Event }
 
     public Type type = Type.Lerp;
     public float timestamp;
     public float value;
     public String eventName;
+
+    public int CompareTo(KeyFrame other) {
+        return timestamp.CompareTo(other.timestamp);
+    }
 }
 
 public class AiAnimation : ScriptableObject {
