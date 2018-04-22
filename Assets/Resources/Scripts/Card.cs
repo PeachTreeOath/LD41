80cad79da09c3c8e27a7d3d642f83d69571c967a
Unknown <wjackson2112@gmail.com>
2018-04-22 17:26:16 -0600

    Scroll the text on the card for super long card names

--- Assets/Resources/Scripts/Card.cs
+++ Assets/Resources/Scripts/Card.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System;
 using TMPro;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CardView))]
 public class Card : MonoBehaviour
@@ -404,21 +405,87 @@ public class Card : MonoBehaviour
         }
 
         string cardName = cardModel.name;
+        string shortCardName = cardModel.name;
         TextMeshProUGUI cardTextField = cardView.nameText;
+
+        Debug.Log("Input");
+
         if (cardName.Equals(inputText, StringComparison.CurrentCultureIgnoreCase))
         {
             MatchedWord();
         }
         else if (inputText.Length > 0 && cardName.StartsWith(inputText, StringComparison.CurrentCultureIgnoreCase))
         {
+            shortCardName = cardModel.name;
+
             string highlightedLetters = cardName.Substring(0, inputText.Length);
             string restOfWord = cardName.Substring(inputText.Length);
             cardTextField.text = "<color=blue>" + highlightedLetters + "</color><color=white>" + restOfWord + "</color>";
+
+            float textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+            float bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+
+            int frontNumToRemove = 0;
+            int backNumToRemove = 0;
+
+            //This makes it so we always show 10 highlighted characters then the rest are not highlighted
+            while(textWidth > bannerWidth && shortCardName.Substring(0, inputText.Length - frontNumToRemove).Length > 10)
+            {
+                //Remove a character from the front
+                frontNumToRemove++;
+                shortCardName = shortCardName.Substring(1);
+
+                //Recalculate size
+                highlightedLetters = shortCardName.Substring(0, inputText.Length - frontNumToRemove);
+                restOfWord = shortCardName.Substring(inputText.Length - frontNumToRemove);
+                cardTextField.text = "<color=blue>" + highlightedLetters + "</color><color=white>" + restOfWord + "..." + "</color>";
+                textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+                bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+            }
+
+            //Remove more from the back till it fits
+            while(textWidth > bannerWidth)
+            {
+                //Remove a character from the back
+                backNumToRemove++;
+                shortCardName = shortCardName.Substring(0, shortCardName.Length - 1);
+
+                //Recalculate size
+                highlightedLetters = shortCardName.Substring(0, inputText.Length - frontNumToRemove);
+                restOfWord = shortCardName.Substring(inputText.Length - frontNumToRemove);
+                cardTextField.text = "<color=blue>" + highlightedLetters + "</color><color=white>" + restOfWord + "..." + "</color>";
+                textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+                bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+            }
+
+            //If there are no more to remove
+            if (backNumToRemove == 0)
+            {
+                //Get rid of the ...
+                highlightedLetters = shortCardName.Substring(0, inputText.Length - frontNumToRemove);
+                restOfWord = shortCardName.Substring(inputText.Length - frontNumToRemove);
+                cardTextField.text = "<color=blue>" + highlightedLetters + "</color><color=white>" + restOfWord + "</color>";
+                textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+                bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+            }
+
         }
         else if (inputText.Length > 0)
         {
             //User has typed something but it doesn't match anything, we need to force a backspace
             inputText = inputText.Substring(0, inputText.Length - 1);
+        }
+        else
+        {
+            float textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+            float bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+            while(textWidth > bannerWidth){
+                Debug.Log("Remove " + shortCardName[shortCardName.Length - 1]);
+                shortCardName = shortCardName.Substring(0, shortCardName.Length - 1);
+                cardTextField.text = shortCardName + "...";
+                textWidth = LayoutUtility.GetPreferredWidth(cardTextField.rectTransform);
+                bannerWidth = ((cardTextField.transform.parent) as RectTransform).rect.width;
+            }
         }
 
     }
